2e814d6b889e9db21ce06fc67dc3b993eda7e290
Benjamin Brugghe <Brugghe.b@gmail.com>
2022-12-28 11:12:21 +0100

    Feat: création composant Router :sparkles:

---
+++ front-end-netflix/src/Components/Router.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from '../Views/HomePage';
+import Error404 from '../Views/Error404';
+import Login from '../Views/Login';
+import Register from '../Views/Register';
+import Feed from '../Views/Feed';
+import UserAccount from '../Views/UserAccount';
+import ManageUsers from '../Views/ManageUsers';
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/register' element={<Register />} />
+                <Route path='/feed' element={<Feed />} />
+                <Route path='/userAccount' element={<UserAccount />} />
+                <Route path='/manageUsers' element={<ManageUsers />} />
+                <Route path='/*' element={<Error404 />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default Router;
