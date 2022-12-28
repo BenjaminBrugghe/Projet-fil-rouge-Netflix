a885d6e8a181895d4510bbdb34f391a123bc45a3
Benjamin Brugghe <Brugghe.b@gmail.com>
2022-12-28 12:21:34 +0100

    Feat: Ajout des Regex pour le formulaire d'inscription (Register.jsx)

--- front-end-netflix/src/Views/Register.jsx
+++ front-end-netflix/src/Views/Register.jsx
@@ -9,14 +9,31 @@ import RegisterPopUp from '../Components/RegisterPopUp';
 
 const Register = () => {
 
+    // Liste des utilisateurs
     const [userList, setUserList] = useState([]);
+
+    // Pour récupérer les informations de l'utilisateur
     const [lastname, setLastname] = useState('');
     const [firstname, setFirstname] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+
+    // Pour afficher ou non les fenêtres
     const [showRegisterForm, setShowRegisterForm] = useState(true);
     const [showSuccess, setShowSuccess] = useState(false);
+
+    // Pour vérification des Regex
+    const [lastnameRegexOK, setLastnameRegexOK] = useState(false);
+    const [firstnameRegexOK, setFirstnameRegexOK] = useState(false);
+    const [emailRegexOK, setEmailRegexOK] = useState(false);
+    const [passwordRegexOK, setPasswordRegexOK] = useState(false);
+    const [confirmPasswordRegexOK, setConfirmPassWordRegexOk] = useState(false);
+
+    // Regex pour les inputs
+    const regexName = /^[a-zA-ZÀ-ÿ\s\-]{3,}$/; // 3 caractères minimum, lettres, espaces et tirets
+    const regexEmail = /^[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$/; // Format email
+    const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/; // 8 caractères, 1 majuscule, 1 minuscule, 1 chiffre, 1 caractère spécial
 
     /**
      * Récupère la liste des utilisateurs
@@ -47,31 +64,78 @@ const Register = () => {
 
         console.log(`*****${userList}*****`); // Affiche [object Object],[object Object]
 
+        // Vérification des Regex
+        checkLastnameRegex(lastname);
+        checkFirstnameRegex(firstname);
+        checkEmailRegex(email);
+        checkPasswordRegex(password);
+        checkConfirmPasswordRegex(confirmPassword);
+
         // Pour vérifier si tous les champs sont remplis
         if (lastname != '' && firstname != '' && email != '' && password != '' && confirmPassword != '') {
-            userList.foreach((el) => {
-                if (email == el.email) { // Pour vérifier que l'adresse email n'est pas déjà utilisée => ERREUR (not a function)
-                    alert('Erreur ! Cette adresse email est déjà associée à un compte !');
-                }
-                else {
-                    if (password != confirmPassword) {
-                        alert('Erreur ! Les deux mots de passe doivent correspondre');
+            if (password != confirmPassword) {
+                alert('Erreur ! Les deux mots de passe doivent correspondre');
+            }
+            else {
+                if (lastnameRegexOK && firstnameRegexOK && emailRegexOK && passwordRegexOK && confirmPasswordRegexOK) {
+                    const newUser = {
+                        lastname: lastname,
+                        firstname: firstname,
+                        email: email,
+                        password: password
                     }
-                    else {
-                        const newUser = {
-                            lastname: lastname,
-                            firstname: firstname,
-                            email: email,
-                            password: password
-                        }
-                        createUser(newUser);
-                        registerSuccess();
-                    }
+                    createUser(newUser);
+                    registerSuccess();
                 }
-            })
+            }
         }
         else {
             alert('Erreur ! Veuillez remplir tout les champs');
+        }
+    };
+
+    const checkLastnameRegex = () => {
+        if (regexName.test(lastname)) {
+            setLastnameRegexOK(true);
+        }
+        else {
+            alert("Erreur ! Le nom doit contenir au moins 3 caractères")
+        }
+    };
+
+    const checkFirstnameRegex = () => {
+        if (regexName.test(firstname)) {
+            setFirstnameRegexOK(true);
+        }
+        else {
+            alert("Erreur ! Le prénom doit contenir au moins 3 caractères")
+        }
+    };
+
+    const checkEmailRegex = () => {
+        if (regexEmail.test(email)) {
+            setEmailRegexOK(true);
+        }
+        else {
+            alert("Erreur ! L'adresse email n'est pas valide (Format : Example@email.com)")
+        }
+    };
+
+    const checkPasswordRegex = () => {
+        if (regexPassword.test(password)) {
+            setPasswordRegexOK(true);
+        }
+        else {
+            alert("Erreur ! Le mot de passe doit contenir au moins 8 caractères, 1 majuscule, 1 minuscule, 1 chiffre et 1 caractère spécial")
+        }
+    };
+
+    const checkConfirmPasswordRegex = () => {
+        if (regexPassword.test(confirmPassword)) {
+            setConfirmPassWordRegexOk(true);
+        }
+        else {
+            alert("Erreur ! Le mot de passe doit contenir au moins 8 caractères, 1 majuscule, 1 minuscule, 1 chiffre et 1 caractère spécial")
         }
     };
 
