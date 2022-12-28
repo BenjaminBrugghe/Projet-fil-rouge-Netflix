a5beca4ea1d64fa3ef9e3baef2dc1af555049fbe
Benjamin Brugghe <Brugghe.b@gmail.com>
2022-12-28 11:12:46 +0100

    Refactor: ajout du composant Router

--- front-end-netflix/src/App.js
+++ front-end-netflix/src/App.js
@@ -1,31 +1,14 @@
 import './App.css';
 import './Styles/Main.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from './Components/Footer';
-import HomePage from './Views/HomePage';
-import Error404 from './Views/Error404';
-import Login from './Views/Login';
-import Register from './Views/Register';
-import Feed from './Views/Feed';
-import UserAccount from './Views/UserAccount';
-import ManageUsers from './Views/ManageUsers';
+import Router from './Components/Router';
+
 
 function App() {
-
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/feed' element={<Feed />} />
-          <Route path='/userAccount' element={<UserAccount />} />
-          <Route path='/manageUsers' element={<ManageUsers />} />
-          <Route path='/*' element={<Error404 />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <Router />
+      <Footer />
     </div>
   );
 }
