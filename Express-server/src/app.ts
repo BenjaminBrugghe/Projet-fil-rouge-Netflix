5877107e338b16a67afde5fe0d18c8336b309c81
Benjamin Brugghe <Brugghe.b@gmail.com>
2023-01-08 16:37:47 +0100

    Fix: Installation et ajout de CORS dans App.ts pour éviter les erreurs

--- Express-server/src/app.ts
+++ Express-server/src/app.ts
@@ -1,19 +1,20 @@
 import express, { Request, Response } from "express";
 import router from "./router/router";
-
 import "dotenv/config";
-const bodyParser = require("body-parser");
 
-// Instance d'Express
+// Instances des dépendances
 const app = express();
-
-// Instance d'une variable d'environnement
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const port = process.env.PORT;
+
+// Middleware pour les CORS
+app.use(cors());
 
 // Pour la persistance en JSON
 app.use(bodyParser.json());
 
-// Middleware pour req.body.xxx (à mettre avant les autres app.use)
+// Middleware pour req.body.xxx
 app.use(express.json());
 
 // A chaque fois que j'interroge app.ts, je passerais par le router
