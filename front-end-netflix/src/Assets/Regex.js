676595f1b20efb0c36d8159848a49dd6671334d3
Benjamin Brugghe <Brugghe.b@gmail.com>
2023-01-08 18:40:00 +0100

    Fix: Ajout de la vérification 2 mots de passe correspondent dans Regex.js

--- front-end-netflix/src/Assets/Regex.js
+++ front-end-netflix/src/Assets/Regex.js
@@ -3,62 +3,51 @@ const regexName = /^[a-zA-ZÀ-ÿ\s\-]{3,}$/; // 3 caractères minimum, lettres, 
 const regexEmail = /^[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$/; // Format email
 const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/; // 8 caractères, 1 majuscule, 1 minuscule, 1 chiffre, 1 caractère spécial
 
-/**
+
+export default class Regex {
+
+    /**
      * Vérifie que le nom contient au moins 3 caractères et pas de nombres
      */
-export const checkLastnameRegex = (lastname) => {
-    if (regexName.test(lastname)) {
-        return true;
-    }
-    else {
-        return false;
-    }
-};
+    checkLastnameRegex = (lastname) => {
+        if (regexName.test(lastname)) {
+            return true;
+        }
+    };
 
-/**
- * Vérifie que le prénom contient au moins 3 caractères et pas de nombres
- */
-export const checkFirstnameRegex = (firstname) => {
-    if (regexName.test(firstname)) {
-        return true;
-    }
-    else {
-        return false;
-    }
-};
+    /**
+     * Vérifie que le prénom contient au moins 3 caractères et pas de nombres
+    */
+    checkFirstnameRegex = (firstname) => {
+        if (regexName.test(firstname)) {
+            return true;
+        }
+    };
 
-/**
- * Vérifie que l'email est au bon format
- */
-export const checkEmailRegex = (email) => {
-    if (regexEmail.test(email)) {
-        return true;
-    }
-    else {
-        return false;
-    }
-};
+    /**
+     * Vérifie que l'email est au bon format
+    */
+    checkEmailRegex = (email) => {
+        if (regexEmail.test(email)) {
+            return true;
+        }
+    };
 
-/**
- * Vérifie que le mot de passe contient au moins 8 caractères, 1 majuscule, 1 minuscule, 1 chiffre et 1 caractère spécial
- */
-export const checkPasswordRegex = (password) => {
-    if (regexPassword.test(password)) {
-        return true;
-    }
-    else {
-        return false;
-    }
-};
+    /**
+     * Vérifie que le mot de passe contient au moins 8 caractères, 1 majuscule, 1 minuscule, 1 chiffre et 1 caractère spécial
+    */
+    checkPasswordRegex = (password) => {
+        if (regexPassword.test(password)) {
+            return true;
+        }
+    };
 
-/**
- * Vérifie que le 2ème mot de passe correspond au 1er
- */
-export const checkConfirmPasswordRegex = (confirmPassword) => {
-    if (regexPassword.test(confirmPassword)) {
-        return true;
-    }
-    else {
-        return false;
-    }
-};
+    /**
+     * Vérifie que les 2 mots de passe correspondent
+    */
+    checkConfirmPasswordRegex = (password, confirmPassword) => {
+        if (password == confirmPassword) {
+            return true;
+        }
+    };
+}
