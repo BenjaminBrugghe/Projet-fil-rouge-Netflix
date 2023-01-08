f439416e363b7d18406b96fa015337b3171ffdcb
Benjamin Brugghe <Brugghe.b@gmail.com>
2023-01-08 18:38:16 +0100

    Refactor: Composant Register.jsx avec plusieurs fonctions + createNewUser()

--- front-end-netflix/src/Views/Register.jsx
+++ front-end-netflix/src/Views/Register.jsx
@@ -3,14 +3,14 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
-import { getAllUsers, createUser } from '../Assets/ApiServices';
+import Service from '../Assets/ApiServices';
 import RegisterPopUp from '../Components/RegisterPopUp';
-import { checkLastnameRegex, checkFirstnameRegex, checkEmailRegex, checkPasswordRegex, checkConfirmPasswordRegex } from '../Assets/Regex';
+import Regex from '../Assets/Regex';
 
 const Register = () => {
 
-    // Liste des utilisateurs
-    const [userList, setUserList] = useState([]);
+    const _regex = new Regex();
+    const _service = new Service();
 
     // Pour récupérer les informations de l'utilisateur
     const [lastname, setLastname] = useState('');
@@ -33,89 +33,99 @@ const Register = () => {
     // Pour vérifier que l'email n'est pas déjà utilisé
     const [emailAlreadyUsed, setEmailAlreadyUsed] = useState(true);
 
-    /**
-     * Récupère la liste des utilisateurs
-     */
-    useEffect(() => {
-        setUserList(getAllUsers());
-        console.table(userList);
-    }, []);
-
-    /**
-     * Affiche une fenêtre avec un message affichant le succès de la création du compte.
-     */
+    // Affiche une fenêtre avec un message affichant le succès de la création du compte.
     function registerSuccess() {
         setShowRegisterForm(!showRegisterForm);
         setShowSuccess(!showSuccess);
     };
 
     /**
-     * Lance les vérifications afin d'enregister l'utilisateur.
-     * Affiche une fenêtre avec un message en cas d'erreur dans le formulaire.
+     * Effectue les vérifications pour les regex
+     * @param {string} lastname Le nom saisi par l'utilisateur
+     * @param {string} firstname Le prénom saisi par l'utilisateur
+     * @param {string} email L'email saisi par l'utilisateur
+     * @param {string} password Le mot de passe saisi par l'utilisateur
+     * @param {string} confirmPassword La confirmation du mot de passe saisie par l'utilisateur
+     */
+    function checkRegex(lastname, firstname, email, password, confirmPassword) {
+        setLastnameRegexOK(_regex.checkLastnameRegex(lastname));
+        setFirstnameRegexOK(_regex.checkFirstnameRegex(firstname));
+        setEmailRegexOK(_regex.checkEmailRegex(email));
+        setPasswordRegexOK(_regex.checkPasswordRegex(password));
+        setConfirmPassWordRegexOk(_regex.checkConfirmPasswordRegex(password, confirmPassword));
+    };
+
+    /**
+     * Affiche une alerte en fonction de la regex qui n'est pas respectée
+     */
+    function regexAlert() {
+        switch (undefined) {
+            case lastnameRegexOK:
+                alert('Erreur ! Le nom doit contenir au moins 3 caractères');
+                break;
+            case firstnameRegexOK:
+                alert('Erreur ! Le prénom doit contenir au moins 3 caractères');
+                break;
+            case emailRegexOK:
+                alert("Erreur ! L'adresse email n'est pas valide (Format : Example@email.com)");
+                break;
+            case passwordRegexOK:
+                alert('Erreur ! Le mot de passe doit contenir au moins 8 caractères, 1 majuscule, 1 minuscule, 1 chiffre et 1 caractère spécial');
+                break;
+            case confirmPasswordRegexOK:
+                alert('Erreur ! Les deux mots de passe doivent correspondre');
+                break;
+            default:
+                break;
+        }
+    };
+
+    /**
+     * Vérifie que l'email saisi par l'utilisateur n'est pas déjà utilisé et affiche une alerte si c'est le cas
+     * @param {User[]} userList La liste des utilisateurs
+     * @param {string} email L'email saisi par l'utilisateur
+     */
+    async function checkEmail(email) {
+        const emailAlreadyExists = await _service.getUserByEmail(email);
+        if (emailAlreadyExists) {
+            alert("Erreur ! L'adresse email est déjà utilisée");
+            setEmailAlreadyUsed(true);
+        }
+        else {
+            setEmailAlreadyUsed(false);
+        }
+    };
+
+    /**
+     * Effectue les vérifications pour les regex et vérifie que l'email n'est pas déjà utilisé.
+     * Si tout est correct, crée l'utilisateur et affiche un message de succès.
      */
     function registerClick(event) {
         event.preventDefault();
 
-        // Vérification des Regex
-        setLastnameRegexOK(checkLastnameRegex(lastname));
-        setFirstnameRegexOK(checkFirstnameRegex(firstname));
-        setEmailRegexOK(checkEmailRegex(email));
-        setPasswordRegexOK(checkPasswordRegex(password));
-        setConfirmPassWordRegexOk(checkConfirmPasswordRegex(confirmPassword));
+        // Vérification des Regex et correspondance des 2 mots de passe
+        checkRegex(lastname, firstname, email, password, confirmPassword);
 
-        // Vérifie que l'addresse email n'est pas déjà utilisée
-        userList.map((user) => {
-            if (user.email == email) {
-                return alert('Erreur ! Cette adresse email est déjà utilisée');
-            }
-            else {
-                return setEmailAlreadyUsed(false);
-            }
-        });
-
-        // Si l'email n'est pas déjà utilisé, vérifie que les mots de passe correspondent
-        if (emailAlreadyUsed == false) {
-            if (password != confirmPassword) {
-                alert('Erreur ! Les deux mots de passe doivent correspondre');
-            }
-            else { // Si les mots de passe correspondent, vérifie que les Regex sont OK
-                if (lastnameRegexOK && firstnameRegexOK && emailRegexOK && passwordRegexOK && confirmPasswordRegexOK) {
-                    const newUser = {
-                        lastname: lastname,
-                        firstname: firstname,
-                        email: email,
-                        password: password
-                    }
-                    // Crée l'utilisateur et affiche un message de succès
-                    createUser(newUser);
-                    registerSuccess();
-                }
-                else {
-                    switch (false) {
-                        case lastnameRegexOK:
-                            alert('Erreur ! Le nom doit contenir au moins 3 caractères');
-                            break;
-                        case firstnameRegexOK:
-                            alert('Erreur ! Le prénom doit contenir au moins 3 caractères');
-                            break;
-                        case emailRegexOK:
-                            alert("Erreur ! L'adresse email n'est pas valide (Format : Example@email.com)");
-                            break;
-                        case passwordRegexOK:
-                            alert('Erreur ! Le mot de passe doit contenir au moins 8 caractères, 1 majuscule, 1 minuscule, 1 chiffre et 1 caractère spécial');
-                            break;
-                        case confirmPasswordRegexOK:
-                            alert('Erreur ! Les deux mots de passe doivent correspondre');
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            }
+        // Si tous les champs sont corrects, vérifie que l'email n'est pas déjà utilisé
+        if (lastnameRegexOK && firstnameRegexOK && emailRegexOK && passwordRegexOK && confirmPasswordRegexOK) {
+            checkEmail(email);
         }
+        else { // Si un des champs n'est pas correct, affiche une alerte
+            regexAlert();
+        };
+
+        // Si l'email n'est pas déjà utilisé, crée l'utilisateur et affiche un message de succès
+        if (emailAlreadyUsed == false) {
+            const newUser = {
+                lastname: lastname,
+                firstname: firstname,
+                email: email,
+                password: password
+            }
+            _service.createNewUser(newUser);
+            registerSuccess();
+        };
     };
-
-
 
     return (
         <div className='register'>
